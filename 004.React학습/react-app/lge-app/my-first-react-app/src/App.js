dd072d868d82acd73a63c273fd7d965a4da300d6
imlimge <limge12@gmail.com>
2023-11-08 13:20:08 +0900

    231108_1320

--- 004.React학습/react-app/lge-app/my-first-react-app/src/App.js
+++ 004.React학습/react-app/lge-app/my-first-react-app/src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import $ from 'jquery';
+window.jQuery = $;
+require('jquery-ui-dist/jquery-ui');
+require('jquery-ui-touch-punch/jquery.ui.touch-punch');
+
 
 function App() {
+  let isrc = './images/dcm36.jpeg';
+
+  //렌더링 후 실행구역
+  useEffect(()=>{
+    $('.App-header').click(()=>{
+      window.open('http://www.naver.com')
+    })
+    // 제이쿼리 드래그
+    $('h1').draggable();
+
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={isrc} style={{borderRadius:'50%'}} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +35,7 @@ function App() {
         >
          ㅠㅠㅠㅠㅠㅠㅠㅠㅠ
         </a>
+        <h1>리액트리액트</h1>
       </header>
     </div>
   );
