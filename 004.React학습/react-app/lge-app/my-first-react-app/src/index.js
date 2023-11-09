29b75c87c128c35d29eb22b9ba37418fe3eaac10
imlimge <limge12@gmail.com>
2023-11-09 12:15:17 +0900

    231109_1215

--- 004.React학습/react-app/lge-app/my-first-react-app/src/index.js
+++ 004.React학습/react-app/lge-app/my-first-react-app/src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// index.js는 public/index.html 페이지에 적용되는 컴포넌트다 -> 루트컴포넌트
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { TopArea } from "./dc/TopArea";
+// css도 불러온다
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App(){
+  return(
+    <TopArea />
+  )
+}
+
+
+
+// 컴포넌트 출력 /////////
+// 먼저 root 객체만들고
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+// render 메서드로 출력
+root.render(<App />);
+
