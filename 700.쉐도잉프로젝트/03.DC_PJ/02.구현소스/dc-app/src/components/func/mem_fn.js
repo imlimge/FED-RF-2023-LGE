0c2af0cb3be7f3278fb088fbfbfd82f317fc110d
imlimge <limge12@gmail.com>
2023-12-18 16:34:13 +0900

    231218_1634

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/func/mem_fn.js
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/func/mem_fn.js
@@ -17,10 +17,24 @@ const initData = () => {
                   [
                       {
                           "idx": "1",
+                          "uid":"admin",
+                          "pwd":"1111",
+                          "unm":"Administrator",
+                          "eml":"admin@dc.com"
+                      },
+                      {
+                          "idx": "2",
                           "uid":"tomtom",
                           "pwd":"1111",
                           "unm":"Tom",
                           "eml":"tom@gmail.com"
+                      },
+                      {
+                          "idx": "3",
+                          "uid":"meme",
+                          "pwd":"1111",
+                          "unm":"meme",
+                          "eml":"meme@gmail.com"
                       }
                   ]
               `
--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/Layout.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/Layout.jsx
@@ -10,18 +10,17 @@ import { useNavigate } from "react-router-dom";
 import { useCallback, useLayoutEffect, useState } from "react";
 
 export function Layout() {
-
-
-  // ****************** Hook 상태관리 변수 ************ //
-  // 1. 로그인상태 체크변수 : 로컬스 'minfo' 초기할당
-  const [logSts,setLogSts] = useState(localStorage.getItem('minfo'));
-
+  
+  // ********* Hook 상태관리 변수 ********* //
+  // 1. 로그인 상태체크 변수 : 로컬스 'minfo'초기할당!
+  const [logSts,setLogSts] = 
+  useState(localStorage.getItem('minfo'));
   // 2. 로그인 환영메시지 상태변수
   const [logMsg,setLogMsg] = useState(null);
 
-  /////////////////////////////////////
-  // 3. 로그아웃함수 ///////////////////
-  // -> TopArea 컴포넌트에 전달함
+  /////////////////////////////
+  ////// 로그아웃 함수 /////////
+  // -> TopArea 컴포넌트에 전달함!
   const logOut = useCallback(()=>{
     // 1. 로컬스 삭제(minfo)
     localStorage.removeItem('minfo');
@@ -30,12 +29,8 @@ export function Layout() {
     // 3. 로그인 메시지 업데이트
     setLogMsg(null);
     // 4. 첫페이지로 이동
-    chgPage('/',{})
-    
-  },[]); /// logOUt 함수 ////
-
-
-
+    // chgPage('/',{});
+  },[]); //////// logOut함수 //////////
 
   // 랜더링 후(화면보이기전) 실행구역 //////////
   useLayoutEffect(()=>{
@@ -47,7 +42,8 @@ export function Layout() {
   const goNav = useNavigate();
 
   // 라우터 이동함수 : pgName - 페이지이름 / param - 전달값
-  const chgPage = useCallback((pgName,param) => goNav(pgName,param),[]);
+  const chgPage = useCallback(
+    (pgName,param) => goNav(pgName,param),[]);
   // 메모이제이션 되는 TopArea 컴포넌트에 제공하는 함수가
   // useCallback을 사용한 메모이제이션 처리되어야 변경없는 것을
   // 체크하여 함수를 업데이트 하지 않는다!
@@ -56,16 +52,22 @@ export function Layout() {
 
   /********************************** 
    [컨텍스트 API 공유값 설정]
-   1. chgPage 함수 : 라우터 이동기능   
-   2. setLogSts : 로그인 상태값 업데이드
-   3. setLogMsg : 로그인 메시지 업데이트
+   1. chgPage 함수 : 라우터 이동기능 
+   2. logSts : 로그인 상태값
+   3. setLogSts : 로그인 상태값 업데이트  
+   4. setLogMsg : 로그인 메시지 업데이트  
    **********************************/
   // 리턴코드 ////////////////////////
   return (
-    <dcCon.Provider value={{ chgPage ,setLogSts, setLogMsg }}>
+    <dcCon.Provider value={{ chgPage, logSts, setLogSts, setLogMsg }}>
       {/* 메모이제이션 관리를 위해 함수를
       컨텍스트방식이 아닌 속성으로 직접보냄! */}
-      <TopArea chgPageFn={chgPage} logSts={logSts} logMsg={logMsg} logOut={logOut}/>
+      <TopArea 
+        chgPageFn={chgPage} 
+        logSts={logSts} 
+        logMsg={logMsg}
+        logOut={logOut}
+      />
       <MainArea />
       <FooterArea />
     </dcCon.Provider>
