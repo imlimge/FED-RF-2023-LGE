69ac22bd4acffd0457b326dc0f059a3a3d1e8bfb
imlimge <limge12@gmail.com>
2024-01-12 18:15:17 +0900

    240112_1815

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/Fashion.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/Fashion.jsx
@@ -17,6 +17,14 @@ import { ItemDetail } from "../modules/ItemDetail";
 // 부드러운 스크롤 JS
 import { scrolled, setPos } from "../func/smoothScroll24";
 
+
+// 리액트용 패럴랙스 - 설치 : npm i react-parallax
+import { Parallax } from "react-parallax";
+import { gnbData } from "../data/gnb";
+// 설명 : https://www.npmjs.com/package/react-parallax
+
+
+
 export function Fashion(props) {
   // 컨텍스트 API 사용!
   const myCon = useContext(pCon);
@@ -41,7 +49,7 @@ export function Fashion(props) {
     // 에 대한 기본 막기가 가능함!(여기서는 스크롤 기능임!)
 
     // 부드러운 스크롤 위치값 초기화!!!
-    setPos(0);
+    // setPos(0);
 
     // 스크롤바 생성하기(x축은 숨김)
     $("html,body").css({
@@ -69,11 +77,19 @@ export function Fashion(props) {
       // 부드러운 스크롤 위치값 초기화!!!
       setPos(0);
     }; /////// 소멸자 ////////////////
+
   }, []); ///////// useEffect ///////////
 
+  // props.cat 카테고리가 변경될때만 맨위로 값 변경!
   useLayoutEffect(()=>{
+    console.log('같으면 실행안함?');
+    // 부드러운 스크롤 위치값
     setPos(0);
-  },[props.cat])
+    // 윈도우 실제로 상단이동
+    window.scrollTo(0, 0);
+    // 열렸을 수 있는 상세페이지 닫기
+    $('.bgbx').hide();
+  },[props.cat]);
 
   
 
@@ -106,8 +122,22 @@ export function Fashion(props) {
       <div className="bgbx">
         <ItemDetail goods={item} cat={props.cat} />
       </div>
-      {/* 3. 패럴랙스 영역 */}
-      <section id="c2" className="cont c2 men"></section>
+      {/* 3. 패럴랙스 영역 : 리액트용 패럴랙스 적용 */}
+      <section id="c2" className="cont">
+        <Parallax 
+          className="c2"
+          // 패럴랙스할 배경이미지 설정속성 bgImage
+          bgImage={"./images/sub/"+props.cat+"/02.special.png"}
+          // 패럴랙스 이동정도 조정속성 strength
+          // 수치범위 : -500 ~ 1000 -> 높은 숫자는 반대방향
+          strength={200}
+          >
+            <h2 class="c2tit">
+                2023 {gnbData[props.cat][1]}
+            </h2>
+        </Parallax>
+
+      </section>
       {/* 4. 단일상품영역 */}
       <section id="c3" className="cont c3"></section>
       {/* 5. 스타일상품영역 */}
--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
@@ -1,21 +1,17 @@
 // 메인 페이지 컨텐츠 컴포넌트
 
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 import { Banner } from "../modules/Banner";
 
-
 // 자동스크롤 JS 불러오기
-// import { autoScroll } from "../func/jquery-autoScroll";
-  import { evtFn,initSet,wheelFn,zeroPno  } from "../func/jquery-autoScroll";
+import { evtFn, initSet, wheelFn, zeroPno } from "../func/jquery-autoScroll";
 
 // 드래그배너 JS 불러오기
 import { dragBanner } from "../func/drag_banner";
 import { FashionIntro } from "../modules/FashionIntro";
 
-
-// 제이쿼리 호출
-import $ from "jquery";
-
+// 제이쿼리
+import $ from 'jquery';
 
 export function MainCont() {
   // 메인 페이지일때만 자동스크롤 기능 적용함!
@@ -23,59 +19,61 @@ export function MainCont() {
     // 랜더링 후 한번만 적용!
     // console.log("랜더링OK!");
     
-
-    //스크롤바 없애기
+    // 스크롤바 없애기
     $('html,body').css({overflow:'hidden'});
 
-
-
-    // ((중요))
-    // 특정이벤트를 설정해제하고자 할 때
+    // (((중요!!!))) /////////////////////////////
+    // 특정이벤트를 설정해제하고자 할때
     // 반드시 그 이벤트 설정은 JS파일 내부에서 하지 말고
-    // 리액트 함수에서 JS 함수를 호출하는 형태로 해야
-    // 해제 메서드인 removeEventListener 가 유효함
+    // 리액트 함수에서 JS함수를 호출하는 형태로해야
+    // 해제 메서드인 removeEventListener 가 유효함!
 
-
-    //자동스크롤 호출
-    // autoScroll();
+    // 자동스크롤 이벤트 설정하기 /////
     window.addEventListener('wheel',wheelFn);
 
-    //호기화 함수 호출
+    // 메뉴+인디케이터 이벤트 기능설정함수 호출 ////
+    evtFn();
+
+    // 초기화 함수 호출
     initSet();
 
     // 페이지번호 초기화 호출
     zeroPno();
 
-
-    // 메뉴+인디케이터 이벤트 기능설정함수호출
-    evtFn();
-
     //드래그배너 호출
     dragBanner();
 
-
-
-    //컴포넌트 소멸자 : 이 컴포넌트가 삭제될 때 호출
+    // 컴포넌트 소멸자 : 이 컴포넌트가 삭제될때 호출됨 //
     return(()=>{
-      console.log('난소멸')
+      console.log('난 소멸했어~!');
+
+      // 이 페이지에서만 필요했던 자동스크롤 이벤트 해제!
       window.removeEventListener('wheel',wheelFn);
 
-      // 메인페이지에만 사용되는 로고 클릭 시 상단이동 이벤트 해제
-      // 제이쿼리로 특정요소에 걸어준 경우 해제는 off(이벤트명)
+      // 메인 페이지에만 사용되는 로고클릭시 상단이동 이벤트 해제
+      // 제이쿼리로 특정요소에 걸어준경우 해제는 off(이벤트명)
       $("#logo a").off('click');
       $(".gnb li").off('click').removeClass('on');
       $(document).off('keydown');
 
 
-    })
-    
+    });////////// 소멸자 return //////
   }, []); /////// useEffect ///////////
 
+  // 처음 로딩시 스크롤 상단이동 //////
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, []); ///// useLayoutEffect //////////
 
+  // 리턴 코드 /////////////////////
   return (
     <>
       {/* 1. 배너 페이지 */}
-      <section id="ban" className="page nonesel" style={{ background: "lightblue" }}>
+      <section
+        id="ban"
+        className="page none-sel"
+        style={{ background: "lightblue" }}
+      >
         <Banner />
       </section>
 
@@ -88,7 +86,7 @@ export function MainCont() {
       <section className="page">
         <FashionIntro cat="women" />
       </section>
-      
+
       {/* 4. 스타일패션 페이지 */}
       <section className="page">
         <FashionIntro cat="style" />
@@ -96,22 +94,29 @@ export function MainCont() {
 
       {/* 메인에만 나오는 사이드 인디케이터 */}
       <nav className="indic">
-            <ul>
-                <li className="on">
-                    <a href="#ban"><span>배너</span></a>
-                </li>
-                <li>
-                    <a href="#men"><span>남성의류</span></a>
-                </li>
-                <li>
-                    <a href="#women"><span>여성의류</span></a>
-                </li>
-                <li>
-                    <a href="#style"><span>스타일</span></a>
-                </li>
-            </ul>
-        </nav>
-   
+        <ul>
+          <li className="on">
+            <a href="#ban">
+              <span>배너</span>
+            </a>
+          </li>
+          <li>
+            <a href="#men">
+              <span>남성의류</span>
+            </a>
+          </li>
+          <li>
+            <a href="#women">
+              <span>여성의류</span>
+            </a>
+          </li>
+          <li>
+            <a href="#style">
+              <span>스타일</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 } //////// MainCont 컴포넌트 ///////
