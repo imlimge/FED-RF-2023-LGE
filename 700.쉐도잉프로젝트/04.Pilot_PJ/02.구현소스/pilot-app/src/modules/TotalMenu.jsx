06f8733ac8871d99a4ea8b13332acca13e671d9d
imlimge <limge12@gmail.com>
2024-01-16 13:16:51 +0900

    240116_1316

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/TotalMenu.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/TotalMenu.jsx
@@ -1,30 +1,23 @@
 // Pilot PJ 전체메뉴 컴포넌트
 
+// 컨텍스트 API 불러오기
 import { useContext } from "react";
 import { pCon } from "./PilotContext";
 
-
-
-
 export function TotalMenu() {
 
+  // 컨텍스트 사용
+  const myCon = useContext(pCon);
+  // pCon에 Provider value 속성에 공개한 변수/함수를 사용함!
 
+  // 메뉴 이동 처리함수 ///
+  const goPage = (txt) => {
+    myCon.chgPgName(txt);
+    // 전체박스 숨기기
+    document.querySelector('.ham').click();
+  }; //////// goPage 메서드 ///////////
 
-// 컨텍스트 사용
-const myCon = useContext(pCon);
-// pCon에 Provider value 속성에 공개한 변수/함수를 사용함
-
-// 메뉴 이동 처리 함수
-const goPage = (txt) =>{
-  myCon.chgPgName(txt);
-
-  //전체박스 숨기기
-  document.querySelector('.mbox').style.display = 'none';
-
-}
-
-
-
+  // 코드 리턴 //////////////////////
   return (
     <>
       <div className="mbox">
@@ -37,7 +30,11 @@ const goPage = (txt) =>{
         <nav className="mlist">
           <dl>
             <dt>
-              <a href="#" onClick={()=>goPage('men')}>MEN</a>
+              <a href="#" 
+              onClick={(e)=>{
+                e.preventDefault();
+                goPage('men');
+              }}>MEN</a>
             </dt>
             <dd>
               <a href="#">T-SHIRT</a>
@@ -54,7 +51,11 @@ const goPage = (txt) =>{
           </dl>
           <dl>
             <dt>
-              <a href="#" onClick={()=>goPage('women')}>WOMEN</a>
+              <a href="#" 
+              onClick={(e)=>{
+                e.preventDefault();
+                goPage('women');
+              }}>WOMEN</a>
             </dt>
             <dd>
               <a href="#">T-SHIRT</a>
@@ -71,7 +72,11 @@ const goPage = (txt) =>{
           </dl>
           <dl>
             <dt>
-              <a href="#" onClick={()=>goPage('style')}>STYLE</a>
+              <a href="#" 
+              onClick={(e)=>{
+                e.preventDefault();
+                goPage('style');
+              }}>STYLE</a>
             </dt>
             <dd>
               <a href="#">COLLECTION</a>
@@ -90,4 +95,4 @@ const goPage = (txt) =>{
       </div>
     </>
   );
-} ///////// TotalMenu 컴포넌트 /////////////
+} ///////// TotalMenu 컴포넌트 //////////
